37e29fa88ca885e2ee9bc8b0decdee421f0ac571
Nguyen Thang <thangn.1411@gmail.com>
2017-09-09 19:44:10 +0700

    Update client.go

--- client.go
+++ client.go
@@ -17,6 +17,9 @@ var apiURL = "https://core.icheck.com.vn"
 var apiDevURL = "http://sandbox.icheck.com.vn:4336"
 var Dev = true
 
+var AppID string 
+var Secret string
+
 type Backend interface {
 	Call(method, path string, form *RequestValues, params *Params, v interface{}) error
 }
